2d3edb813b63600ea7051e17563fa8085ba97a57
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-04 00:18:31 -0700

    react

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
 
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>ClickMe</button>
-      <Link to="/">Back to Home Page</Link>
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
+        
       </BrowserRouter>
     </div>
   );
