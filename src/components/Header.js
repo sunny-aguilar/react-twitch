a546313e03ddaea8502b66e85e10939152c4f370
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-04 00:37:26 -0700

    react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <div className="right menu">
-        Header
+        <Link to="/" className="item">Streamer</Link>
       </div>
     </div>
   );
