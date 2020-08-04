4fecf96edf4b7584b3ac050a3dc8608101f05dd6
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-04 00:19:47 -0700

    react

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
+import StreamCreate from "./streams/StreamCreate";
+import StreamCreate from "./streams/StreamDelete";
+import StreamCreate from "./streams/StreamEdit";
+import StreamCreate from "./streams/StreamList";
+import StreamCreate from "./streams/StreamShow";
 
 
 const App = () => {
