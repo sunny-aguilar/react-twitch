e2457b2e0527f6b4c8e8fd9a80fbeb8599eea487
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-04 00:22:56 -0700

    react

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ const App = () => {
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
