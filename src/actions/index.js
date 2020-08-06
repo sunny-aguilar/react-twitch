9de9d00c5dba6e6561f3f08302e977b1a9be438b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-06 16:19:43 -0700

    react

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,5 +16,6 @@ export const signOut = () => {
 
 export const createStream = (formValues) => async (dispatch) => {
   const response = await streams.post("/streams", formValues);
+
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
