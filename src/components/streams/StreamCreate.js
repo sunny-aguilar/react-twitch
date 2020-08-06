72be389f431cbdde607d235444b60ed40d25a851
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-05 22:30:28 -0700

    react

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -14,7 +14,6 @@ class StreamCreate extends React.Component {
     }
   }
 
-  
   renderInput = ({ input, label, meta }) => {
     const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
