3ed41c61f2242a59b8f180d0c26c9fc1a4735150
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-06 21:58:41 -0700

    react

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
-
+import streamReducer from "./streaReducer";
 
 export default combineReducers({
   auth: authReducer,
