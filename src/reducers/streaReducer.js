6761f754bcf752517c1f8c757efd2207093e65aa
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-06 21:41:36 -0700

    react

--- src/reducers/streaReducer.js
+++ src/reducers/streaReducer.js
@@ -9,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case "FETCH_STREAMS":
+      return {};
     case "FETCH_STREAM":
       return { ...state, [action.payload.id]: action.payload };
     case "CREATE_STREAM":
