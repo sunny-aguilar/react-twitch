fd868a9bfee8b2e33410918e51ac1901c5a600c9
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-06 22:17:22 -0700

    react

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,7 +12,10 @@ class StreamList extends React.Component {
       return (
         <div className="item" key={stream.id}>
           <i className="large middle aligned icon camera" />
-          
+          <div className="content">
+            {stream.title}
+            <div className="description">{ stream.description }</div>
+          </div>
         </div>
       );
     });
