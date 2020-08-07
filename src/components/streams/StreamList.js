bd6968b1b353bc11fcfcd6b1801edb71e7101d80
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-06 22:06:38 -0700

    react

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -2,8 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+  componentDidMount() {
+    
+  }
+}
 
 export default connect(null, { fetchStreams })(StreamList);
