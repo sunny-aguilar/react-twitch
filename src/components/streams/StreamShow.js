ae5b6a86f8afde56ddcdf622460c08ad710bee38
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-10 14:36:20 -0700

    react

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -24,7 +24,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        <video ref={this.videoRef} style={{ width: "100%"}} />
+        <video ref={this.videoRef} style={{ width: "100%"}} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
